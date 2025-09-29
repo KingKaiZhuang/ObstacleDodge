3e4f48401c7fa942bb36fd4d2d695f1ceacf8172
KingKaiZhuang <5b0g0024@stust.edu.tw>
2025-09-29 20:52:15 +0800

    Using OnCollisionEnter

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -7,15 +7,26 @@ public class NewBehaviourScript : MonoBehaviour
     [SerializeField] float moveSpeed = 10f;
     void Start()
     {
-
+        PrintInstruction();
     }
 
     void Update()
     {
-        
+        MovePlayer();
+    }
+
+    void PrintInstruction()
+    {
+        Debug.Log("Welcome to the game. Use WASD or arrow keys to move the player");
+        Debug.Log("Move using arrow keys or WASD");
+        Debug.Log("Don't bump into objects!");
+    }
+
+    void MovePlayer()
+    {
         float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
         float yValue = 0f;
         float zValue = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
-        transform.Translate(xValue,yValue,zValue);
+        transform.Translate(xValue, yValue, zValue);
     }
 }
