c815f4d833def55f6074b21708bdb6a6135e8fbd
KingKaiZhuang <5b0g0024@stust.edu.tw>
2025-09-29 18:02:14 +0800

    first commit

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 10f;
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        
+        float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+        float yValue = 0f;
+        float zValue = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+        transform.Translate(xValue,yValue,zValue);
+    }
+}
